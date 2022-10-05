0a9df0caf93a081e983c194a433f23968f99ff40
”0xAureoleNode“ <aureolenode@gmail.com>
2022-10-05 15:27:41 +0800

    component switch to react.createElement

--- pratice-pure-react/src/index.js
+++ pratice-pure-react/src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function HelloWorld() {
-  return <div>Hello World!</div>;
+// function MyThing() {
+//   return (
+//     <div className="book">
+//       <div className="title">The Title</div>
+//       <div className="author">The Author</div>
+//       <ul className="stars">
+//         <li className="rating">5 stars</li>
+//         <li className="isbn">12-345678-910</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+function Mything() {
+  return React.createElement(
+    'div',
+    { className: 'book' },
+    React.createElement('div', { className: 'title' }, 'The Title'),
+    React.createElement('div', { className: 'author' }, 'The Author'),
+    React.createElement(
+      'ul',
+      { className: 'stars' },
+      React.createElement('li', { className: 'rating' }, '5 stars'),
+      React.createElement('li', { className: 'isbn' }, '12-345678-910')
+    )
+  );
 }
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<Mything />, document.getElementById('root'));
